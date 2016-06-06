6c921c8ce24b4b38622e41bd1df8e95a023a5116
mun625 <mun625@gmail.com>
2016-06-06 15:16:34 +0200

    STEP3. Plot the data using ggplot2

--- read.r
+++ read.r
@@ -1,3 +1,4 @@
+library(ggplot2)
 
 # STEP1. Read CSV data and summarize it to step1.txt
 seaflow <- read.csv(file="seaflow_21min.csv",head=TRUE,sep=",")
@@ -5,8 +6,13 @@ sink("step1.txt")
 print(summary(seaflow))
 sink()
 
-
 # STEP2. Split the data into traing and test sets
 dt = sort(sample(nrow(seaflow), nrow(seaflow)*.7))
 train <- seaflow[dt,]
 test <- seaflow[-dt,]
+
+# STEP3. Plot the data
+pe_plot <- ggplot(seaflow, aes(x=chl_small, y=pe)) + geom_point(aes(colour=pop, group=pop))
+save(pe_plot,file="step3.RData")
+load("step3.RData")
+ggsave("step3.png")
