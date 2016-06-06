d5cccbcf2dcf1af23d74a949d4a11c6e74a92cc1
mun625 <mun625@gmail.com>
2016-06-06 13:49:37 +0200

    STEP2. Split data into training and data sets

--- read.r
+++ read.r
@@ -1,4 +1,12 @@
-heisenberg <- read.csv(file="seaflow_21min.csv",head=TRUE,sep=",")
+
+# STEP1. Read CSV data and summarize it to step1.txt
+seaflow <- read.csv(file="seaflow_21min.csv",head=TRUE,sep=",")
 sink("step1.txt")
-print(summary(heisenberg))
+print(summary(seaflow))
 sink()
+
+
+# STEP2. Split the data into traing and test sets
+dt = sort(sample(nrow(seaflow), nrow(seaflow)*.7))
+train <- seaflow[dt,]
+test <- seaflow[-dt,]
