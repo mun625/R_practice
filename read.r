14c60e517dae7633f002b3578dec4f672a3aa1c4
mun625 <mun625@gmail.com>
2016-06-06 16:52:57 +0200

    STEP4. Train a Decision Tree using rpart built-in function

--- read.r
+++ read.r
@@ -1,4 +1,5 @@
 library(ggplot2)
+library(rpart)
 
 # STEP1. Read CSV data and summarize it to step1.txt
 seaflow <- read.csv(file="seaflow_21min.csv",head=TRUE,sep=",")
@@ -16,3 +17,10 @@ pe_plot <- ggplot(seaflow, aes(x=chl_small, y=pe)) + geom_point(aes(colour=pop, 
 save(pe_plot,file="step3.RData")
 load("step3.RData")
 ggsave("step3.png")
+
+# Step4. Train a decision tree
+sink("step4.txt")
+fol <- formula(pop ~ fsc_small + fsc_perp + fsc_big + pe + chl_big + chl_small)
+model <- rpart(fol, method="class", data=train)
+print(model)
+sink()
