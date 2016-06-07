8d37bafce418273b6a38076cba9a0ea1373d1a97
mun625 <mun625@gmail.com>
2016-06-07 12:25:52 +0200

    STEP7. Train a support vector machine model and compare results

--- read.r
+++ read.r
@@ -1,6 +1,7 @@
 library(ggplot2)
 library(rpart)
 library(randomForest)
+library("e1071")
 
 # STEP1. Read CSV data and summarize it to step1.txt
 seaflow <- read.csv(file="seaflow_21min.csv",head=TRUE,sep=",")
@@ -33,10 +34,19 @@ accuracy <- pred == test$pop
 print(sum(accuracy)/length(accuracy))
 sink()
 
-# Step 6: Build and evaluate a random forest.
+# Step 6: Build and evaluate a random forest
 sink("step6.txt")
 model2 <- randomForest(fol, method="class", data=train)
 pred <- predict(model2, test, type='class')
 accuracy <- pred == test$pop
 print(sum(accuracy)/length(accuracy))
 sink()
+
+# Step 7: Train a support vector machine model and compare results.
+sink("step7.txt")
+model3 <- svm(fol, method="class", data=train)
+pred <- predict(model3, test, type='class')
+accuracy <- pred == test$pop
+print(sum(accuracy)/length(accuracy))
+sink()
+
